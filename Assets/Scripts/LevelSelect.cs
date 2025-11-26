e450ebf74fb4e75de584ad56b73425c285cf0543
ConnorStew8415 <Gamerman6007@gmail.com>
2025-11-26 21:34:53 +1100

    Basic main menu that functions, need to add locked levels and proper settings

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LevelOneSelcet()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LevelTwoSelcet()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LevelThreeSelcet()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+
+}

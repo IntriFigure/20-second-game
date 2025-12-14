1c28b26488fcbaa41d594ae0b966e3fc0c854491
IntriFigure <damientran4@gmail.com>
2025-12-14 21:26:32 +1100

    .

--- Assets/Scripts/Managers/SoundsFXManger.cs
+++ Assets/Scripts/Managers/SoundsFXManger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SoundsFXManger : MonoBehaviour
 {
     public static SoundsFXManger instant;
+    [SerializeField] private AudioSource soundFXObject;
 
     private void Awake()
     {
@@ -14,8 +15,27 @@ public class SoundsFXManger : MonoBehaviour
         }
     }
 
-    public void PlaySoundFXclip(AudioClip audioClip, Transform spawnTransform )
+    public void PlayRandomSoundFXclip(AudioClip[] audioClip, Transform spawnTransform, float volume)
     {
+        //random index
+        int rand = Random.Range(0, audioClip.Length);
 
+        // object spawn 
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        // assign the audio
+        audioSource.clip = audioClip[rand];
+
+        // assign volume 
+        audioSource.volume = volume;
+
+        // play sounds
+        audioSource.Play();
+
+        //get legnth of sound fx clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy clip after finish playing 
+        Destroy(audioSource.gameObject, clipLength);
     }
 }

c94c053e7994e2bd6b53d9f93f6357b2b02f187f
RexsLoserDotAu <duelblade6@gmail.com>
2025-12-15 19:33:15 +1100

    Pizza throw improvement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -71,14 +71,17 @@ public class CameraFollow : MonoBehaviour
         GameObject prefb = Instantiate(pizzaBox, ThrowOrigin.position, Quaternion.identity);
         Rigidbody pizzaRB = prefb.GetComponent<Rigidbody>();
 
-        float shootWVelocity = throwForce * carM.currentCarSpeed;
-        pizzaRB.AddForce(transform.forward * shootWVelocity, ForceMode.Impulse);
+        //float shootWVelocity = throwForce * carM.currentCarSpeed;
+        //pizzaRB.AddForce(transform.forward * shootWVelocity, ForceMode.Impulse);
 
-        /*Vector3 forceDirection = transform.forward;
+        Vector3 forceDirection = transform.forward;
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, 500f))
         {
-            forceDirection = (hit.point - ThrowOrigin.position).normalized;
+            if (!hit.collider.CompareTag("PizzaBox"))
+            {
+                forceDirection = (hit.point - ThrowOrigin.position).normalized;
+            }           
         }
 
         if (pizzaRB != null)
@@ -86,9 +89,8 @@ public class CameraFollow : MonoBehaviour
             float shootWVelocity = throwForce * carM.currentCarSpeed;
             Vector3 forceToAdd = forceDirection * shootWVelocity;// + transform.up * throwUpwardForce;
             pizzaRB.AddForce(forceToAdd, ForceMode.Impulse);
-
-
-        }*/
+        }
+        Debug.Log("nAME"+ hit.collider.gameObject.name);
         Destroy(prefb, 3f);
     }
 
